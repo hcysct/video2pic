96054b7a0bc3a035ff087e78771bf6a359ac5ada
zhaoyonghui <zhaoyonghui@zyhdembp.zte.com.cn>
2017-07-19 23:36:35 +0800

    去掉测试

--- src/main/java/com/demo/video2pic/ffmpeg/FFmpegUtil.java
+++ src/main/java/com/demo/video2pic/ffmpeg/FFmpegUtil.java
@@ -103,13 +103,7 @@ public class FFmpegUtil {
 		}
 		return targetFolder;
 	}
-	
-	public static void main(String[] args) {
-		String a="123\\";
-		System.out.println(a.indexOf('\\'));
-		System.out.println(a.length());
-		System.out.println(a.substring(0, a.length()-2));
-	}
+
 	
 	/**
 	 * 获取视频内容
